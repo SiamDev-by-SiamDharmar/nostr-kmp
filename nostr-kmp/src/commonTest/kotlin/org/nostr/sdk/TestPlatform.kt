84924d60a6a0b925e454e6b79461ae5637ef5c29
rushmi0 <gcalku@gmail.com>
2025-06-11 08:42:10 +0700

    add: common unit test

--- nostr-kmp/src/commonTest/kotlin/org/nostr/sdk/TestPlatform.kt
+++ nostr-kmp/src/commonTest/kotlin/org/nostr/sdk/TestPlatform.kt
@@ -1,7 +1,15 @@
 package org.nostr.sdk
 
+import kotlin.test.Test
+import kotlin.test.assertTrue
 
 class PlatformInfoTest {
 
+    @Test
+    fun testPlatformInfo() {
+        val platform = getPlatform().name
+        println(platform)
+        assertTrue(platform.isNotBlank())
+    }
 
 }
